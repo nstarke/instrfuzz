084b0a8236afc280dd818024927a2a77b13d9ede
nstarke <starke.nicholas@gmail.com>
2024-04-01 08:26:14 -0500

    removing extraneous copy pasta'd nasm comments

--- poc.asm
+++ poc.asm
@@ -1,11 +1,3 @@
-; BOOTFUZZ
-; 
-; Copyright (c) 2024 Nicholas Starke
-; https://github.com/nstarke/bootfuzz
-;
-; assemble with `nasm -f bin -o bootfuzz.img bootfuzz.asm`
-; run in qemu: `qemu-system-i386 -fda bootfuzz.img -nographic -accel kvm`
-
 [bits 16]
 org 0x7c00
 
