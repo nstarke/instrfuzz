0cc72448208dbd688e2e02e026498dfa3aa14282
nstarke <starke.nicholas@gmail.com>
2024-04-02 20:16:47 -0500

    escaping OUTDATE and changing test img file extension for consistency

--- test-instruction.sh
+++ test-instruction.sh
@@ -15,9 +15,9 @@ OUTDATE=$(date +'%Y%m%d%H%M%S')
 echo "Testing ${NIBBLE1}${NIBBLE2}"
 
 cat poc.asm | sed "s/\$NBL1/$NIBBLE1/g" | sed "s/\$NBL2/$NIBBLE2/g" > "${OUTDATE}.test.asm"
-nasm -f bin -o "${OUTDATE}.img" "${OUTDATE}.test.asm"
+nasm -f bin -o "${OUTDATE}.test.img" "${OUTDATE}.test.asm"
 
 echo "Booting QEMU to test instruction.  Press CTRL-A + X to break out of QEMU..."
-qemu-system-i386 -boot a -fda $OUTDATE.img -nographic
+qemu-system-i386 -boot a -fda "{$OUTDATE}.test.img" -nographic
 
-echo "Created ${OUTDATE}.img and ${OUTDATE}.test.asm.  Do you want to keep these files?"
+echo "Created ${OUTDATE}.test.img and ${OUTDATE}.test.asm.  Do you want to keep these files?"
